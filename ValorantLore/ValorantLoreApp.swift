6f9d6240bcd414ce717990b6bd9785fbd2592aca
ozpyn <gabrielmingle@gmail.com>
2024-11-19 17:59:20 -0500

    Initial Commit

---
+++ ValorantLore/ValorantLoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ValorantLoreApp.swift
+//  ValorantLore
+//
+//  Created by ozpyn on 11/19/24.
+//
+
+import SwiftUI
+
+@main
+struct ValorantLoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
